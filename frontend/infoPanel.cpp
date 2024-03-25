76ae0562ec7866ec8f19236c6a38f2a53abacae8
Riccardo010101 <riccardo.carta@studenti.unipd.it>
2024-03-25 20:11:31 +0100

    aggiunto resouces, avanzato sensorpanel

--- frontend/infoPanel.cpp
+++ frontend/infoPanel.cpp
@@ -3,13 +3,6 @@
 InfoPanel::InfoPanel(Sensor * s, QWidget* parent)
     : QWidget(parent)
 {
-    QVBoxLayout* layout = new QVBoxLayout(this);
-    layout->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 
-    name= new QLabel("suca");
-    layout->addWidget(name);
-
-    value = new QLabel("no");
-    layout->addWidget(value);
 
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "frontend/mainwindow.h"
-#include <QRadioButton>
 #include <QApplication>
+#include <QRadioButton>
+#include "frontend/mainwindow.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     SensorPanel s;
+
+
+
     MainWindow w(&s);
+
     w.resize(960, 480);
     w.show();
     return a.exec();
