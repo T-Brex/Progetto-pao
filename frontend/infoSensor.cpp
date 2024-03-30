d8a9a339d20204634fc1351dc88b4034f51475f7
Riccardo010101 <riccardo.carta@studenti.unipd.it>
2024-03-30 17:51:25 +0100

    add resources

---
+++ frontend/infoSensor.cpp
@@ -0,0 +1,26 @@
+#include "frontend\infoSensor.h"
+
+InfoSensor::InfoSensor(const Sensor& sensor, QWidget* parent):QWidget(parent)
+{
+
+
+
+}
+
+
+
+/*
+
+
+
+
+ class InfoSensor:public QWidget{
+private:
+    QLabel* name;
+    QLabel* values;
+    QIcon* ico;
+public:
+    InfoSensor(const InfoSensor& s);
+    InfoSensor(const Sensor& s, QWidget* parent = 0);
+
+};*/
